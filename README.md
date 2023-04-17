df7f460c7c5037e98c267fa39ac613b303e3d079
Akriti Srivastava <akisrivastava22@gmail.com>
2023-04-17 21:32:52 +0530

    Tailwind edited and Readme updated

--- README.md
+++ README.md
@@ -3,10 +3,11 @@ This is a simple Movie App
 
 I have used basic languages like-  HTML, Tailwind & JS 
 
-This App is fetching data and displaying data of any movie that is searched on the search bar 
+This App is fetching data and displaying data of any movie that is searched on the search bar. 
+This website is responsive to all screens
 
 ### Checkout the live website [here](https://movie-app-kappa-ruby.vercel.app/)
 
 ## How the website looks like
 
-![desktop](./appgif.gif)
+![desktop](./images/app.gif)
--- app.js
+++ app.js
@@ -34,42 +34,56 @@ async function handleSearch(event) {
   const movieimg = document.getElementById("photo");
   movieimg.src = Poster;
 
+
   const name = document.getElementById("title");
   name.innerHTML = Title;
+
 
   const movieyear = document.getElementById("year");
   movieyear.innerHTML = Year;
 
   const movieRated = document.getElementById("rated");
   movieRated.innerHTML = Rated;
+  document.getElementById("rate").classList.remove("hidden");
+
 
   const movieReleased = document.getElementById("released");
   movieReleased.innerHTML = Released;
+  document.getElementById("release").classList.remove("hidden");
 
   const movieRuntime = document.getElementById("runtime");
   movieRuntime.innerHTML = Runtime;
+  document.getElementById("run").classList.remove("hidden");
 
   const movieGenre = document.getElementById("genre");
   movieGenre.innerHTML = Genre;
+  document.getElementById("gen").classList.remove("hidden");
 
   const movieDirector = document.getElementById("director");
   movieDirector.innerHTML = Director;
+  document.getElementById("direct").classList.remove("hidden");
 
   const movieWriter = document.getElementById("writer");
   movieWriter.innerHTML = Writer;
+  document.getElementById("write").classList.remove("hidden");
 
   const movieActors = document.getElementById("actors");
   movieActors.innerHTML = Actors;
+  document.getElementById("act").classList.remove("hidden");
 
   const moviePlot = document.getElementById("plot");
   moviePlot.innerHTML = Plot;
+  document.getElementById("pl").classList.remove("hidden");
 
   const movieLanguage = document.getElementById("language");
   movieLanguage.innerHTML = Language;
+  document.getElementById("lang").classList.remove("hidden");
 
   const movieCountry = document.getElementById("country");
   movieCountry.innerHTML = Country;
+  document.getElementById("count").classList.remove("hidden");
 
   const movieAwards = document.getElementById("awards");
   movieAwards.innerHTML = Awards;
+  document.getElementById("prizes").classList.remove("hidden");
 }
